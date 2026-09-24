4850748ee98a32b37b71e9f6e2a8b05d0ecc124f
danielmar091 <danmarmar38@alu.edu.gva.es>
2026-09-24 17:24:58 +0200

    javascriptPrimeraPrueba

---
+++ 1-formulario/miscript/script.js
@@ -0,0 +1,58 @@
+/*console.log("Hola mundo! 😂");
+console.warn("ayuda porfavor");
+console.error("error");
+
+//Version ES6 
+
+// var hola = 'Hola'; //variable antigua
+
+let quetal = 'Que\'s tal'; //variable 
+const comoEstas = 'Mal';
+
+guardarDatos()
+
+function guardarDatos(x, y, z){
+    // escribir muerte
+    console.log(x);
+    console.log(quetal);
+   console.log(z);
+
+   return x;
+}
+
+console.log(document.getElementsByTagName('h1'));
+// recoge todos los tag dentro del parentesis, crea un array y te lo muestra
+
+document.getElementById();
+// recoge el id exacto dentro del parentesis y crea un array
+document.querySelector();
+
+document.querySelectorAll();
+// recoge todos los elementos
+*/
+
+
+let formulario = document.getElementById("formulario");
+let nombre = document.getElementById("name");
+let apellido = document.getElementById("surname");
+let sexoHombre = document.getElementById("sexo_hombre");
+let sexoMujer = document.getElementById("sexo_mujer");
+let sexoIvan = document.getElementById("sexo_ivan");
+let nick = document.getElementById("nick");
+let email = document.getElementById("email");
+let mensaje = document.getElementById("message");
+let submit = document.getElementById("submit");
+let reset = document.getElementById("reset");
+
+
+console.log(formulario);
+console.log(nombre);
+console.log(apellido);
+console.log(sexoHombre);
+console.log(sexoMujer);
+console.log(sexoIvan);
+console.log(nick);
+console.log(email);
+console.log(mensaje);
+console.log(submit);
+console.log(reset);
